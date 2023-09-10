ca1ed5c24062c1287cb8835a446cf3a3110652c1
silbenking <silbenking@mail.ru>
2023-09-10 16:12:59 +0300

    add title and description label

---
+++ layoutTask1/layoutTask1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  layoutTask1
+//
+//  Created by Сергей Сырбу on 10.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
