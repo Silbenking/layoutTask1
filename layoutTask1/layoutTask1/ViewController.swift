7e3c422dbde54b9168cc7be958ff615d5cc9db49
silbenking <silbenking@mail.ru>
2023-09-10 16:51:32 +0300

    task done

--- layoutTask1/layoutTask1/ViewController.swift
+++ layoutTask1/layoutTask1/ViewController.swift
@@ -15,11 +15,16 @@ class ViewController: UIViewController {
     //- setupBehavior - для настройки поведения, нажатия, делегаты и т.д.
     enum Constants {
         static let titleFontSize: CGFloat = 30
-        static let descriptionFontSize: CGFloat = 16
+        static let titleLabelOffset: CGFloat = 32
         static let heigthPopupView: CGFloat = 363
         static let widthPopupView: CGFloat = 343
-        static let titleLabelOffset: CGFloat = 32
-        static let descriptionLabelOffset: CGFloat = 16
+        static let descriptionFontSize: CGFloat = 16
+        static let offset: CGFloat = 16
+        static let clickButtonFontSize: CGFloat = 16
+        static let clickButtonlOffset: CGFloat = 45
+        static let clickButtonlHeight: CGFloat = 51
+        static let secondaryFontSize: CGFloat = 16
+        static let secondaryOffset: CGFloat = 18
 
     }
     let popupView: UIView = {
@@ -46,6 +51,25 @@ class ViewController: UIViewController {
         return label
     }()
     
+    let clickButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Click Me", for: .normal)
+        button.backgroundColor = UIColor(red: 93/255, green: 176/255, blue: 117/255, alpha: 1)
+        button.titleLabel?.font = .systemFont(ofSize: Constants.clickButtonFontSize)
+        button.layer.cornerRadius = 25
+        return button
+    }()
+    
+    let secondoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Secondary Action"
+        label.font = .systemFont(ofSize: Constants.secondaryFontSize)
+        label.textColor = UIColor(red: 93/255, green: 176/255, blue: 117/255, alpha: 1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAppearance()
@@ -65,8 +89,19 @@ class ViewController: UIViewController {
         }
         popupView.addSubview(descriptionLabel)
         descriptionLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(Constants.descriptionLabelOffset)
-            make.trailing.leading.equalTo(popupView).inset(Constants.descriptionLabelOffset)
+            make.top.equalTo(titleLabel.snp.bottom).offset(Constants.offset)
+            make.trailing.leading.equalTo(popupView).inset(Constants.offset)
+        }
+        popupView.addSubview(clickButton)
+        clickButton.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(Constants.clickButtonlOffset)
+            make.height.equalTo(Constants.clickButtonlHeight)
+            make.trailing.leading.equalTo(popupView).inset(Constants.offset)
+        }
+        popupView.addSubview(secondoryLabel)
+        secondoryLabel.snp.makeConstraints { make in
+            make.top.equalTo(clickButton.snp.bottom).offset(Constants.secondaryOffset)
+            make.trailing.leading.equalTo(popupView).inset(Constants.offset)
         }
         
     }
@@ -74,7 +109,11 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor(red: 93/255, green: 176/255, blue: 117/255, alpha: 1)
     }
     private func setupBehavior(){
-        
+        clickButton.addTarget(self, action:#selector(tap), for: .touchUpInside)
+    }
+    
+    @objc func tap() {
+        print("tap tap")
     }
 }
 
